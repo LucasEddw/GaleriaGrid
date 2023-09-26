611d0dd994151cbe8f31db58454a84696c4ab1c0
LucasEddw <lucaseduardo31415@gmail.com>
2023-09-26 08:21:17 -0300

    musicas

---
+++ script.js
@@ -0,0 +1,32 @@
+const audio = document.getElementById('audio');
+
+const FukashigiNoCarte = "assets/audio/FukashigiNoCarte.mp3";
+const BlendS = "assets/audio/BlendS.mp3";
+const NarutoEnd1 = "assets/audio/NarutoEnd1.mp3";
+const ChavesAcapulco = "assets/audio/ChavesAcapulco.mp3";
+const OshiNoKo = "assets/audio/OshiNoKo.mp3";
+const SonicFrontiers = "assets/audio/SonicFrontiers.mp3";
+const YoungGirlA = "assets/audio/YoungGirlA.mp3";
+const DokiDokiForever = "assets/audio/DokiDokiForever.mp3";
+const JustMonika = "assets/audio/JustMonika.mp3";
+const YourReality = "assets/audio/YourReality.mp3";
+const SystemOfDown = "assets/audio/SystemOfDown.mp3";
+const RockefellerStreet = "assets/audio/RockefellerStreet.mp3";
+const AlienBlues = "assets/audio/AlienBlues.mp3";
+const CrimesLost = "assets/audio/CrimesLost.mp3";
+const EstrelarRemix = "assets/audio/EstrelarRemix.mp3";
+const SequestroAs11 = "assets/audio/SequestroAs11.mp3";
+const VocaloidGigachad = "assets/audio/VocaloidGigachad.mp3";
+const IGotNoTime = "assets/audio/IGotNoTime.mp3";
+const AmorHospitalar = "assets/audio/AmorHospitalar.mp3";
+const WeLiveWeLoveWeLie = "assets/audio/WeLiveWeLoveWeLie.mp3";
+const EmotionalPrism = "assets/audio/EmotionalPrism.mp3";
+const Aishite = "assets/audio/Aishite.mp3";
+const StayWithMe = "assets/audio/StayWithMe.mp3";
+const ShakeThatAss = "assets/audio/ShakeThatAss.mp3";
+const TheGirlNextDoor = "assets/audio/TheGirlNextDoor.mp3";
+
+
+function play(path){
+    audio.setAttribute('src', path);
+}
