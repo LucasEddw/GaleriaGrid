08e1b2fbe7306ec754686618e200276cb8fe85d8
LucasEddw <lucaseduardo31415@gmail.com>
2023-10-03 09:18:23 -0300

    ogro

--- script.js
+++ script.js
@@ -25,9 +25,30 @@ const Aishite = "assets/audio/Aishite.mp3";
 const StayWithMe = "assets/audio/StayWithMe.mp3";
 const ShakeThatAss = "assets/audio/ShakeThatAss.mp3";
 const TheGirlNextDoor = "assets/audio/TheGirlNextDoor.mp3";
+const Bloodborne = "assets/audio/Bloodborne.mp3";
+const Ayasa = "assets/audio/Ayasa.mp3";
+const ChainsawMan = "assets/audio/ChainsawMan.mp3";
+const GrandeFamilia = "assets/audio/GrandeFamilia.mp3";
+const Hayloft = "assets/audio/Hayloft.mp3";
+const Megalovania = "assets/audio/Megalovania.mp3";
+const Paradise = "assets/audio/Paradise.mp3";
+const Polyphia = "assets/audio/Polyphia.mp3";
+const Summertime = "assets/audio/Summertime.mp3";
+const TBOI = "assets/audio/TBOI.mp3";
+const UniaoFlasco = "assets/audio/UniaoFlasco.mp3";
+const GumiKing = "assets/audio/GumiKing.mp3";
+const BlueArchieve = "assets/audio/BlueArchieve.mp3";
+const Fnaf1 = "assets/audio/Fnaf1.mp3";
+const Fnaf2 = "assets/audio/Fnaf2.mp3";
+const Fnaf3 = "assets/audio/Fnaf3.mp3";
+const FnafSB = "assets/audio/FnafSB.mp3";
+
+const XXXXXXXXXX = "assets/audio/XXXXXXXXXX.mp3";
+
 
 
 function play(path){
     audio.setAttribute('src', path);
+    audio.volume = 0.8;
 }
 
